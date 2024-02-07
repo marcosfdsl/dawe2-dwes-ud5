128bcce8b7485761dabef28115a657d820cc027d
marcosfdsl <marcos.fernandez@live.u-tad.com>
2024-02-07 22:19:26 +0100

    ca5

--- enrutador.js
+++ enrutador.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const manejador = require('./manejador.js');
-const db = require('./db/db.js')
 
 // GET INDEX
 router.get('/', (req, res) => {
@@ -23,88 +22,27 @@ router.post('/salida', (req, res) => {
 
 // GET DATA
 router.get('/api/v1/data', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'Datos recuperados con éxito!',
-        data: db
-    });
+    manejador.getdata(req, res);
 });
 
 // GET DATA ID
 router.get('/api/v1/data/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const data = db.find(d => d.id === id);
-
-    if (data) {
-        res.status(200).send({
-            success: 'true',
-            message: 'Dato recuperado con éxito!',
-            db: data
-        })
-    } else {
-        res.status(404).send({
-            success: 'false',
-            message: 'El dato no existe!',
-        })
-    }
+    manejador.getdataid(req, res);
 })
 
 // POST DATA
 router.post('/api/v1/data', (req, res) => {
-    if (!req.body.titulo || !req.body.descripcion) {
-        res.status(400).send({
-            success: 'false',
-            message: 'Datos: Título y descripción requeridos!'
-        });
-        return;
-    }
-
-    const id = db.length + 1;
-    const data = { id, ...req.body };
-    db.push(data);
-    res.writeHead(201, { 'Cpntent-Type': 'application/json; charset=utf-8' });
-    res.end(JSON.stringify(data));
+    manejador.postdata(req, res);
 });
 
 // PUT DATA ID
 router.put("/api/v1/data/:id", (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const data = db.find(d => d.id === id);
-    if (data) {
-        data.titulo = req.body.titulo ? req.body.titulo : data.titulo;
-        data.fecha = req.body.fecha ? req.body.fecha : data.fecha;
-        data.descripcion = req.body.descripcion ? req.body.descripcion : data.descripcion;
-        data.invitados = req.body.invitados ? req.body.invitados : data.invitados;
-        return res.status(200).send({
-            success: "true",
-            message: "Dato actializado con éxito!",
-            db: data
-        });
-    } else {
-        return res.status(404).send({
-            success: "false",
-            message: "El dato no existe!"
-        });
-    }
+    manejador.putdataid(req, res);
 });
 
 // DELETE DATA ID
 router.delete('/api/v1/data/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const data = db.find(d => d.id === id);
-    if (data) {
-        db.splice(db.indexOf(data), 1);
-        return res.status(200).send({
-            success: "true",
-            message: "Dato eliminado con éxito!",
-            db: data
-        });
-    } else {
-        return res.status(404).send({
-            success: "false",
-            message: "El dato no existe!"
-        });
-    }
+    manejador.deletedataid(req, res);
 })
 
 // GET 404
--- manejador.js
+++ manejador.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const mod_fs = require('fs');
+const db = require('./db/db.js')
 
 function root(res, ruta) {
     res.sendFile(path.join(__dirname, 'html', ruta));
 }
 
-function salida(req, res, ruta) {
+function salida(req, res) {
+    postdata(req, res);
+
     let salida_html = false;
     try {
         salida_html = mod_fs.readFileSync('./html/salida.html', {encoding: 'utf8', flag: 'r'});
@@ -20,5 +23,96 @@ function salida(req, res, ruta) {
     return res.send(salida_html);
 }
 
+// GET DATA
+function getdata(req, res) {
+    res.status(200).send({
+        success: 'true',
+        message: 'Datos recuperados con éxito!',
+        data: db
+    });
+}
+
+// GET DATA ID
+function getdataid(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const data = db.find(d => d.id === id);
+
+    if (data) {
+        res.status(200).send({
+            success: 'true',
+            message: 'Dato recuperado con éxito!',
+            db: data
+        })
+    } else {
+        res.status(404).send({
+            success: 'false',
+            message: 'El dato no existe!',
+        })
+    }
+}
+
+// POST DATA
+function postdata(req, res) {
+    if (!req.body.titulo || !req.body.descripcion) {
+        res.status(400).send({
+            success: 'false',
+            message: 'Datos: Título y descripción requeridos!'
+        });
+        return;
+    }
+
+    const id = db.length + 1;
+    const data = { id, ...req.body };
+    db.push(data);
+    res.writeHead(201, { 'Cpntent-Type': 'application/json; charset=utf-8' });
+    res.end(JSON.stringify(data));
+}
+
+// PUT DATA ID
+function putdataid(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const data = db.find(d => d.id === id);
+    if (data) {
+        data.titulo = req.body.titulo ? req.body.titulo : data.titulo;
+        data.fecha = req.body.fecha ? req.body.fecha : data.fecha;
+        data.descripcion = req.body.descripcion ? req.body.descripcion : data.descripcion;
+        data.invitados = req.body.invitados ? req.body.invitados : data.invitados;
+        return res.status(200).send({
+            success: "true",
+            message: "Dato actializado con éxito!",
+            db: data
+        });
+    } else {
+        return res.status(404).send({
+            success: "false",
+            message: "El dato no existe!"
+        });
+    }
+}
+
+// DELETE DATA ID
+function deletedataid(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const data = db.find(d => d.id === id);
+    if (data) {
+        db.splice(db.indexOf(data), 1);
+        return res.status(200).send({
+            success: "true",
+            message: "Dato eliminado con éxito!",
+            db: data
+        });
+    } else {
+        return res.status(404).send({
+            success: "false",
+            message: "El dato no existe!"
+        });
+    }
+}
+
 exports.root = root;
 exports.salida = salida;
+exports.getdata = getdata;
+exports.getdataid = getdataid;
+exports.postdata = postdata;
+exports.putdataid = putdataid;
+exports.deletedataid = deletedataid;
